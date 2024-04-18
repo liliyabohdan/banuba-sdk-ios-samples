1f561f6c605c54c2f99b3c01d76036e0f4a68601
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-04-18 10:13:20 +0300

    Add video recording

---
+++ video_recording/video_recording/ViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+import BNBSdkApi
+import BNBSdkCore
+
+class VideoRecordingViewController: UIViewController {
+
+    @IBOutlet weak var effectView: EffectPlayerView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    private var player: Player?
+    private let cameraDevice = CameraDevice(cameraMode: .FrontCameraSession, captureSessionPreset: .hd1280x720)
+    private var videoOutput: Video?
+    
+    private var effect: BNBEffect?
+
+    private var fileURL: URL = FileManager.default.temporaryDirectory.appendingPathComponent("video.mp4")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        player = Player()
+        videoOutput = Video(cameraDevice: cameraDevice)
+        
+        player?.use(input: Camera(cameraDevice: cameraDevice), outputs: [effectView, videoOutput!])
+        cameraDevice.start()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        effect = player?.load(effect: "TrollGrandma", sync: true)
+    }
+    
+    @IBAction func closeCamera(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pushRecordButton(_ sender: UIButton) {
+        if videoOutput?.state != .stopped {
+            // Stop video recording
+            videoOutput?.stop()
+            return
+        }
+        
+        videoOutput?.record(url: fileURL, size: player?.size ?? CGSize()) { [weak self] state in
+            switch state {
+            case .recording, .paused:
+                self?.recordButton.setImage(UIImage(named: "stop_video"), for: .normal)
+            case .stopped, .processing:
+                self?.recordButton.setImage(UIImage(named: "shutter_video"), for: .normal)
+            @unknown default:
+                break
+            }
+        } onFinished: { [weak self] success, error in
+            let alert = UIAlertController(title: "Video Recording", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            if let error = error, !success {
+                // Show video recording error
+                alert.message = error.localizedDescription
+            } else {
+                // Propose to save recorded video
+                alert.message = "Would you like to save recorded video in gallery?"
+                alert.addAction(UIAlertAction(title: "Save", style: .default) { action in
+                    self?.saveVideoToGallery()
+                })
+            }
+            self?.present(alert, animated: true, completion: nil)
+        } onProgress: { duration in
+            let time = round(duration)
+            let minutes = Int(time) / 60
+            let seconds = Int(time) % 60
+            let text = String(format: "%02d : %02d", minutes, seconds)
+            print("Video recording in progress: \(text)")
+        }
+    }
+    
+    private func saveVideoToGallery() {
+        if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(fileURL.relativePath) {
+            UISaveVideoAtPathToSavedPhotosAlbum(fileURL.relativePath, nil, nil, nil)
+        }
+    }
+}
