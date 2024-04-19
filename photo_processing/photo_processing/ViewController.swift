c2e0104dd4404711a6e221ffc7bf4002202f5fe9
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-04-19 11:04:56 +0300

    Add photo processing

--- photo_processing/photo_processing/ViewController.swift
+++ photo_processing/photo_processing/ViewController.swift
@@ -1,40 +1,87 @@
 import UIKit
+import Foundation
 import BNBSdkApi
 import BNBSdkCore
 
-class ViewController: UIViewController {
-    // Output surface for the `Player`
+class PhotoEditingViewController: UIViewController {
+    
     @IBOutlet weak var effectView: EffectPlayerView!
+    @IBOutlet weak var selectImageButton: UIButton!
     
-    // Input stream for the `Player`
-    private let cameraDevice = CameraDevice(
-        cameraMode: .FrontCameraSession,
-        captureSessionPreset: .hd1280x720
-    )
+    private lazy var player = Player()
+    private lazy var photo = Photo()
+
+    private let imagePicker = UIImagePickerController()
     
-    // `Player` process frames from the input and render them into the outputs
-    private var player: Player?
-    
-    // Current effect
-    private var effect: BNBEffect?
+    private var makeup: BNBEffect?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        player = Player()
+        player.effectPlayer.effectManager()?.add(self)
+        player.use(input: photo, outputs: [effectView])
         
-        // Connect `CameraDevice` and `EffectPlayerView` to `Player`
-        player?.use(input: Camera(cameraDevice: cameraDevice))
-        player?.use(outputs: [effectView])
+        // Load Makeup effect from `effects` folder
+        makeup = player.load(effect: "Makeup")
         
-        // Load effect from `effects` folder
-        effect = player?.load(effect: "TrollGrandma")
+        // Initialize Makeup effect with the Hair.color and the Eyes.color
+        makeup?.evalJs("Hair.color('1.0 0.0 0.0 0.5');", resultCallback: nil)
+        makeup?.evalJs("Eyes.color('1.0 0.0 0.0 0.5');", resultCallback: nil)
         
-        // Start feeding frames from camera
-        cameraDevice.start()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player.effectPlayer.effectManager()?.remove(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
+    @IBAction func selectImage(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func hairValueChanged(_ sender: UISlider) {
+        makeup?.evalJs("Hair.color('1.0 \(sender.value / 10) 0.0 0.5')", resultCallback: nil)
+    }
+    
+    @IBAction func eyesValueChanged(_ sender: UISlider) {
+        makeup?.evalJs("Eyes.color('1.0 \(sender.value / 10) 0.0 0.5')", resultCallback: nil)
+    }
+    
     @IBAction func closeCamera(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoEditingViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.effectView.contentMode = .scaleAspectFit
+            self.photo.take(from: pickedImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoEditingViewController: BNBErrorListener {
+    func onError(_ domain: String, message: String) {
+        DispatchQueue.main.async { [weak self] in
+            self?.selectImageButton.isHidden = true
+            self?.dismiss(animated: true, completion: nil)
+            let alert = UIAlertController(
+                title: "Can't load the Makeup effect",
+                message: "This demo requires the Makeup effect in the 'effects' folder. You can find information about how to get the effect in README.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self?.present(alert, animated: false, completion: nil)
+        }
     }
 }
